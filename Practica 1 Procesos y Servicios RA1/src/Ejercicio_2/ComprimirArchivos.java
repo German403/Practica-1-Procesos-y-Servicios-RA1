ffc3bc166469f21d9ba0316e25bab5f86dd0d649
Germán <122525043+German403@users.noreply.github.com>
2024-10-25 15:05:35 +0200

    Update ComprimirArchivos.java

--- Practica 1 Procesos y Servicios RA1/src/Ejercicio_2/ComprimirArchivos.java
+++ Practica 1 Procesos y Servicios RA1/src/Ejercicio_2/ComprimirArchivos.java
@@ -2,36 +2,46 @@ package Ejercicio_2;
 
 import java.io.*;
 
+/**
+ *
+ * @author GERMÁN ESCUDERO RODRÍGUEZ
+ * @version 1
+ * 20/10/24
+ */
 public class ComprimirArchivos {
 
+    /**
+     * Metodo compresorArchivos,
+     * @param archivosAComprimir
+     * @param tar
+     * @throws IOException
+     */
     public static void compresorArchivos(String[] archivosAComprimir, String tar) throws IOException {
         try {
-            ProcessBuilder pb = new ProcessBuilder("tar", "-cvf", tar, "-T", "-");
-
-            Process p = pb.start();
-
+            Process p = new ProcessBuilder("tar", "-cvf", tar, "-T", "-").start();
 
             try(BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()))) {
                 for (String archivo : archivosAComprimir) {
                     bf.write(archivo);
                     bf.newLine();
                 }
+                bf.flush();
             } catch (IOException e) {
                 throw new RuntimeException(e);
-            }
+            }//Fin try-catch
 
             BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String linea;
             System.out.println("Archivos comprimidos:");
             while ((linea = br.readLine()) != null) {
                 System.out.println(linea);
-            }
+            }//Fin while
 
-            BufferedReader er = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-            StringBuilder eO = new StringBuilder();
-            while ((linea = er.readLine()) != null) {
-                eO.append(linea).append("\n");
-            }
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+            StringBuilder sb = new StringBuilder();
+            while ((linea = br2.readLine()) != null) {
+                sb.append(linea).append("\n");
+            }//Fin while
 
             int codigoSalida = p.waitFor();
             System.out.println("Resultado: " + codigoSalida);
@@ -39,14 +49,12 @@ public class ComprimirArchivos {
                 System.out.println("Compresión exitosa en " + tar);
             } else {
                 System.out.println("Error al comprimir los archivos:");
-                System.out.println(eO.toString());
-            }
-
-
+                System.out.println(sb.toString());
+            }//Fin if-else
         } catch(IOException | InterruptedException e){
             e.printStackTrace();
-        }
-    }
+        }//Fin try-catch
+    }//Fin metodo compresorArchivos
 
     public static void comprobacion_Creacion_Archivos(String[] archivosAComprimir) throws IOException {
         for (String archivo : archivosAComprimir) {
