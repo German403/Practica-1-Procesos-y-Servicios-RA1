125d42d747f1985092ced9bfa72b3be89e09ba64
Germán <122525043+German403@users.noreply.github.com>
2024-10-25 15:04:50 +0200

    Update EjecutarComandos.java

--- Practica 1 Procesos y Servicios RA1/src/Ejercicio_1/EjecutarComandos.java
+++ Practica 1 Procesos y Servicios RA1/src/Ejercicio_1/EjecutarComandos.java
@@ -1,35 +1,60 @@
 package Ejercicio_1;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.*;//clase para creacion y manipulacion de archivos
+import java.util.InputMismatchException;
+import java.util.Scanner;//clase para entrada de datos por teclado para el metodo menu()
 
-//encapsula la funcionalidad de ejecutar comandos del sistema operativo utilizando ProcessBuilder.
-//ProcessBuilder apartado 1.2.2 pagina 24
+/**
+ * Clase EjecutarComandos que encapsula la funcionalidad de ejecutar comandos
+ * del sistema operativo utilizando ProcessBuilder.
+ * Proporciona métodos para ejecutar comandos con diferentes tipos de redirección.
+ * @author GERMÁN ESCUDERO RODRÍGUEZ
+ * @version 1
+ * 19/10/24
+ */
 
 public class EjecutarComandos {
 
+    /**
+     * Metodo menu para seleccionar que opcion de ejecucion del comando desea el usuario
+     * @return opcion elegida mediante entrada por teclado
+     */
     public static int menu() {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int opcion;
-        try {
-            System.out.println("Seleccione una de las tres opciones: " +
-                    "\n1 para realizar un comando" +
-                    "\n2 para " +
-                    "\n3 para realizar un comando y enviar la salida a un archivo");
-            opcion = scanner.nextInt();
-            if (opcion < 1 || opcion > 3) {
-                throw new Exception("Opcion no valida");
+        while (true) {
+            try {
+                System.out.println("Seleccione una de las tres opciones: " +
+                        "\n1 para realizar un comando" +
+                        "\n2 para realizar un comando mediante el uso de un buffer" +
+                        "\n3 para realizar un comando y enviar la salida a un archivo");
+                opcion = sc.nextInt();
+
+                // Validar la opción
+                if (opcion < 1 || opcion > 3) {
+                    throw new IllegalArgumentException("Opción no válida, debe ser entre 1 y 3.");
+                }
+
+                // Retornar la opción válida
+                return opcion;
+
+            } catch (InputMismatchException e) {
+                System.err.println("Error: Debes introducir un número entero.");
+                sc.next(); // Limpiar el buffer
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
             }
-            return opcion;
-        } catch (Exception e1) {
-            e1.printStackTrace();
-            return -1;
         }
+    }//Fin metodo menu
 
-
-    }
-
-    //Ejecuta el comando especificado y devuelve el código de salida del proceso
+    /**
+     * Metodo ejecutarComando, ejecutara el comando  especificado y devolvera el código de salida del proceso
+     * Lo ejecutara mediante el cmd y sera un comando que se cerrara tras la ejecucion
+     * @param comando que se ejecutara
+     * @return codigo de salida para comprobar si el comando se ha efectuado correctamente
+     * @throws IOException en caso de error en la entrada o salida del fichero
+     * @throws InterruptedException en caso de que el metodo se interrumpa durante la espera
+     */
     public int ejecutarComando(String comando) throws IOException, InterruptedException {
         try {
             Process proceso1 = new ProcessBuilder("cmd", "/c",comando).start();
@@ -38,11 +63,19 @@ public class EjecutarComandos {
         }catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return 1;
-        }
+        }//Fin try-catch
+    }//Fin metodo ejecutarComando
 
-    }
-
-    //Ejecuta el comando y proporciona una entrada estándar al proceso.
+    /**
+     * Metodo ejecutarComandoEntrada, ejecutara el comando especificado y proporcionara una entrada estandar al proceso
+     * La entrada se enviara mediante el uso de outputStream para interactuar con el proceso
+     * Lo ejecutara mediante el cmd y sera un comando que se cerrara tras la ejecucion
+     * @param comando que se ejecutara
+     * @param entrada estandar para enviar al proceso
+     * @return codigo de salida para comprobar si el comando se ha efectuado correctamente
+     * @throws IOException en caso de error en la entrada o salida del fichero
+     * @throws InterruptedException en caso de que el metodo se interrumpa durante la espera
+     */
     public int ejecutarComandoEntrada(String comando, String entrada) throws IOException, InterruptedException {
         try {
             Process proceso2 = new ProcessBuilder("cmd", "/c",comando).start();
@@ -58,11 +91,19 @@ public class EjecutarComandos {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return 1;
-        }
+        }//Fin try-catch
+    }//Fin metodo ejecutarComandoEntrada
 
-    }
-
-    //Ejecuta el comando y redirige la salida estándar a un archivo
+    /**
+     * Metodo ejecutarComandoRedireccion, ejecutara el comando especificado y redirigira la salida estandar a un archivo
+     * La salida del proceso sera escrita en el archivo
+     * Lo ejecutara mediante el cmd y sera un comando que se cerrara tras la ejecucion
+     * @param comando que se ejecutara
+     * @param archivoSalida donde se redirigirá la salida del proceso
+     * @return codigo de salida para comprobar si el comando se ha efectuado correctamente
+     * @throws IOException en caso de error en la entrada o salida del fichero
+     * @throws InterruptedException en caso de que el metodo se interrumpa durante la espera
+     */
     public int ejecutarComandoRedireccion(String comando, File archivoSalida) throws IOException, InterruptedException {
         try {
             ProcessBuilder pb = new ProcessBuilder("cmd", "/c",comando);
@@ -74,43 +115,52 @@ public class EjecutarComandos {
         }catch (IOException | InterruptedException e) {
             e.printStackTrace();
             return 1;
-        }
-    }
+        }//Fin try-catch
+    }//Fin metodo ejecutarComandoRedireccion
 
+    /**
+     * Metodo main de la clase EjecutarComandos, tomara la opcion seleccionada del menu y ejecutara
+     * el comando correspondiente
+     * @param args
+     * @throws IOException en caso de error en la entrada o salida del fichero
+     * @throws InterruptedException en caso de que el metodo se interrumpa durante la espera
+     */
     public static void main(String[] args) throws IOException, InterruptedException {
         EjecutarComandos ejecutar = new EjecutarComandos();
-
         switch (menu()){
-            case 1:
+            case 1://Ejecuta un comando
                 int resultado1 = ejecutar.ejecutarComando("echo Prueba primer proceso");
+                System.out.println("Resultado: " + resultado1);
                 if (resultado1 == 0){
-                    System.out.println("Resultado: " + resultado1);
                     System.out.println("El comando se ejecuto correctamente");
                 } else {
                     System.err.println("Hubo un problema en la ejecucion");
-                }
+                }//Fin if-else
                 break;
-            case 2:
+            case 2://Ejecuta un comando con entrada estandar
                 int resultado2 = ejecutar.ejecutarComandoEntrada("echo", "Prueba segundo proceso");
+                System.out.println("Resultado: " + resultado2);
                 if (resultado2 == 0){
-                    System.out.println("Resultado: " + resultado2);
                     System.out.println("El comando se ejecuto correctamente");
                 } else {
                     System.err.println("Hubo un problema en la ejecucion");
-                }
+                }//Fin if-else
                 break;
-            case 3:
-                File archivoSalida = new File("src\\Ejercicio_1\\archivoSalida.txt");
-                int resultado3 = ejecutar.ejecutarComandoRedireccion("echo Prueba tercer proceso", archivoSalida);
-                if (resultado3 == 0){
-                    System.out.println("Resultado: " + resultado3);
-                    System.out.println("El comando se ejecuto correctamente");
-                } else {
-                    System.err.println("Hubo un problema en la ejecucion");
-                }
-                break;
-        }
+            case 3://Ejecuta un comando y redirige la salida a un archivo
 
+                //Rutas del archivo, diferentes segun el sistema operativo por problemas con la lectura
+                //File archivoSalida = new File("Practica 1 Procesos y Servicios RA1\\src\\Ejercicio_1\\archivoSalida.txt");//windows 10
+                File archivoSalida = new File("src\\Ejercicio_1\\archivoSalida.txt");//windows 11
+
+                int resultado3 = ejecutar.ejecutarComandoRedireccion("echo Prueba tercer proceso", archivoSalida);
+                System.out.println("Resultado: " + resultado3);
+                if (resultado3 == 0){
+                    System.out.println("El comando se ejecuto correctamente");
+                } else {
+                    System.err.println("Hubo un problema en la ejecucion");
+                }//Fin if-else
+                break;
+        }//Fin swtich
         System.out.println("Cerrando el programa");
-    }
-}
+    }//Fin metodo main
+}//Fin clase ejecutarComando
